d68e40f60ec63912bd1d817671e7b54f0e18fe27
Andres <softwareapgm@gmail.com>
2020-09-28 17:54:38 -0400

    Updated "sec1" method; Updated JSON file numeration
    Sec1: modified to read JSON file by sections and rows.

--- GuiSetUp.py
+++ GuiSetUp.py
@@ -5,21 +5,38 @@ import json
 
 #---------------------- GUI RETRIVAL ------------------------
 
-def sec1(gui_config):
+def sec1(gui_config,section,rows=0):
     section_widgets = []
-    #Parse Widgets Section 
-    for widget in gui_config["1"]["1"]:
-        for properties in gui_config["1"]["1"][widget]:
-            if gui_config["1"]["1"][widget][properties] == "Menu":
-                if gui_config["1"]["1"][widget]["callFunction"] == True:
-                    #Get Clients
-                    section_widgets.append(get_clients())
-                    break
-            if widget in section_widgets:
-                continue
-            else:
-                section_widgets.append(widget)
-    
+    section = str(section)
+    #Parse Widgets Section
+    if rows == 0:
+        row = str(rows)
+        for widget in gui_config[section][row]:
+            for properties in gui_config[section][row][widget]:
+                if gui_config[section][row][widget][properties] == "Menu":
+                    if gui_config[section][row][widget]["callFunction"] == True:
+                        #Get Clients
+                        section_widgets.append(get_clients())
+                        break
+                if widget in section_widgets:
+                    continue
+                else:
+                    section_widgets.append(widget)
+    else:
+        for r in range(rows):
+            row = str(r)
+            for widget in gui_config[section][row]:
+                for properties in gui_config[section][row][widget]:
+                    if gui_config[section][row][widget][properties] == "Menu":
+                        if gui_config[section][row][widget]["callFunction"] == True:
+                            #Get Clients
+                            section_widgets.append(get_clients())
+                            break
+                    if widget in section_widgets:
+                        continue
+                    else:
+                        section_widgets.append(widget)
+
     return section_widgets
 
 
@@ -39,8 +56,6 @@ def get_lable_settings(*listofprofiles):
 
     return profiles
 
-
-
 def tt_(listofprofiles):
     profiles   = list()
 
@@ -59,13 +74,17 @@ with open("./widgetsSetup.json") as config:
     gui_File = json.load(config)
 
 
-F_sec_0 = tt_(sec1(gui_File))
+F_sec_0 = tt_(sec1(gui_File,1,1))
 #F_sec_0=get_lable_settings("Client   ", get_clients(), "New Client")
 
-F_sec_1=get_lable_settings("Job ID","Job Name","Type of Book","Number of Copies","Number of Pages","Number of Printers")
+F_sec_1 = tt_(sec1(gui_File,2,6))
 #F_sec_1=get_lable_settings("Job ID","Job Name","Type of Book","Number of Copies","Number of Pages","Number of Printers")
 
-F_sec_2=get_lable_settings("Type of Siding","Single","Double")
+print(sec1(gui_File,3,2))
+F_sec_2 = tt_(sec1(gui_File,3,2))
+#F_sec_2=get_lable_settings("Type of Siding","Single","Double")
+
+
 F_sec_3= get_lable_settings("PROFILES SELECTION", "Paper", paperPfl().get_profilesnames(), see_profile, "Printer", ["TOSHIBA","CANON"],see_profile )
 F_sec_4 = get_lable_settings("Extras","Binding",["None","Wire","Plastic Coil","Plastic Combo"], "Inserts")
 F_sec_5 = get_lable_settings("Submitt")
